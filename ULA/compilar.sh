fb11eb9d91f79c092ff68a5196e1e4bea432de0a
Rafael Bergo <rafaelbergo503@gmail.com>
2024-11-04 22:42:32 -0300

    Removido lab e modificado script

--- ULA/compilar.sh
+++
@@ -1,9 +0,0 @@
-ghdl -a ula.vhd
-ghdl -e ula
-
-ghdl -a ula_tb.vhd
-ghdl -e ula_tb
-
-ghdl -r ula_tb --wave=ula_tb.ghw
-
-gtkwave ula_tb.ghw
--- compiler.sh
+++ compiler.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
+    echo "Uso: ./compilar.sh <name>"
     exit 1
 fi
 
 FILE_NAME="$1"
 
-ghdl -a "${FILE_NAME}.vhdl"
+ghdl -a "${FILE_NAME}.vhd"
 ghdl -e "${FILE_NAME}"
 
-ghdl -a "${FILE_NAME}_tb.vhdl"
+ghdl -a "${FILE_NAME}_tb.vhd"
 ghdl -e "${FILE_NAME}_tb"
 
 ghdl -r "${FILE_NAME}_tb" --wave="${FILE_NAME}_tb.ghw"
 
 gtkwave "${FILE_NAME}_tb.ghw"
-
-rm -rf "${FILE_NAME}.ghw"
-rm -rf "${FILE_NAME}_tb.ghw"
-rm -rf "work-obj93.cf"
