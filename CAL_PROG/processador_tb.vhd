5cb3434775feddd5c11dadf5d0f6aa0a41a9ecc1
Rafael Bergo <rafaelbergo503@gmail.com>
2024-12-05 16:51:23 -0300

    separado processador do testbench

--- CAL_PROG/processador_tb.vhd
+++ CAL_PROG/processador_tb.vhd
@@ -1,8 +1,3 @@
--- Giovane Limas Salvi - 2355841 - s71
--- Rafael Carvalho Bergo - 2387190 - s71
-
-------------------------------------------------------------------
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -11,114 +6,57 @@ entity processador_tb is
 end;
 
 architecture a_processador_tb of processador_tb is
-    
-    component fetch is port (
-        clk                 : in std_logic;
-        rst                 : in std_logic;
-        rd_rom              : in std_logic;
-        wr_pc               : in std_logic;
-        jump_en             : in std_logic;
-        instruction         : out unsigned(16 downto 0)
-    );
-    end component;
+    -- Constantes
+    constant period_time : time := 100 ns;
 
-    component decode is port (
-        clk             : in std_logic;
-        rst             : in std_logic;
-        instruction     : in unsigned(16 downto 0);
-        rd_rom          : out std_logic;
-        wr_pc           : out std_logic;
-        jump_en         : out std_logic;
-        operation       : out unsigned(1 downto 0);
-        entr1           : out unsigned(15 downto 0);
-        entr0           : out unsigned(15 downto 0);
-        ula_out         : in unsigned(15 downto 0)
-    );
-    end component;
+    -- Sinais principais
+    signal clk           : std_logic := '0';
+    signal rst           : std_logic := '1';
+    signal finished      : std_logic := '0';
 
-    component execute is port (
-        clk             : in std_logic;
-        rst             : in std_logic;
-        entr0           : in unsigned(15 downto 0);
-        entr1           : in unsigned(15 downto 0);
-        operation       : in unsigned(1 downto 0);
-        result          : out unsigned(15 downto 0)
-    );
+    -- Declaração do componente
+    component processador is
+        port (
+            clk : in std_logic;
+            rst : in std_logic
+        );
     end component;
-
-    constant period_time            : time := 100 ns;
-    signal finished                 : std_logic := '0';
-    signal clk, rst                 : std_logic;
-    signal wr_pc, rd_rom            : std_logic;
-    signal instruction              : unsigned(16 downto 0);
-    signal result                   : unsigned(15 downto 0);
-    signal operation                : unsigned(1 downto 0);
-    signal entr1, entr0             : unsigned(15 downto 0);
-    signal jump_en                  : std_logic;
 
 begin
+    -- Instância do processador
+    uut: processador
+        port map(
+            clk => clk,
+            rst => rst
+        );
 
-    fetch_uut : fetch port map(
-        clk => clk,
-        rst => rst,
-        rd_rom => rd_rom,
-        wr_pc => wr_pc,
-        jump_en => jump_en,
-        instruction => instruction
-    );
-
-    decode_uut : decode port map(
-        clk => clk,
-        rst => rst,
-        rd_rom => rd_rom,
-        wr_pc => wr_pc,
-        jump_en => jump_en,
-        instruction => instruction,
-        operation => operation,
-        entr1 => entr1,
-        entr0 => entr0,
-        ula_out => result
-    );
-
-    execute_uut : execute port map(
-        clk => clk,
-        rst => rst,
-        entr0 => entr0,
-        entr1 => entr1,
-        operation => operation,
-        result => result
-    );
-
+    -- Reset global
     reset_global: process
     begin
-        rst <= '1';
-        wait for period_time*2;
-        rst <= '0';
+        rst <= '1';                          -- Ativa o reset
+        wait for period_time * 2;            -- Aguarda 2 ciclos de clock
+        rst <= '0';                          -- Desativa o reset
         wait;
     end process;
 
+    -- Tempo total de simulação
     sim_time_proc: process
     begin
-        wait for 10 us;         -- <== TEMPO TOTAL DA SIMULACAO!!!
-        finished <= '1';
+        wait for 10 us;                      -- Define o tempo total de simulação
+        finished <= '1';                     -- Marca o término da simulação
         wait;
-    end process sim_time_proc;
+    end process;
 
+    -- Geração do clock
     clk_proc: process
-    begin                       -- gera clock até que sim_time_proc termine
+    begin
         while finished /= '1' loop
             clk <= '0';
-            wait for period_time/2;
+            wait for period_time / 2;        -- Meio período do clock
             clk <= '1';
-            wait for period_time/2;
+            wait for period_time / 2;        -- Meio período do clock
         end loop;
         wait;
     end process clk_proc;
 
-    process
-    begin
-        wait for 200 ns;
-        
-        wait;
-    end process;
 end architecture;
