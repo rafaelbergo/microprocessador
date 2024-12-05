cab136136e9de0c8d59f5c66f6b242839a53b579
Rafael Bergo <rafaelbergo503@gmail.com>
2024-12-05 16:54:38 -0300

    corrigido testbench

--- CAL_PROG/processador_tb.vhd
+++ CAL_PROG/processador_tb.vhd
@@ -6,15 +6,12 @@ entity processador_tb is
 end;
 
 architecture a_processador_tb of processador_tb is
-    -- Constantes
     constant period_time : time := 100 ns;
 
-    -- Sinais principais
-    signal clk           : std_logic := '0';
-    signal rst           : std_logic := '1';
+    signal clk           : std_logic;
+    signal rst           : std_logic;
     signal finished      : std_logic := '0';
 
-    -- Declaração do componente
     component processador is
         port (
             clk : in std_logic;
@@ -23,40 +20,42 @@ architecture a_processador_tb of processador_tb is
     end component;
 
 begin
-    -- Instância do processador
     uut: processador
         port map(
             clk => clk,
             rst => rst
         );
 
-    -- Reset global
     reset_global: process
     begin
-        rst <= '1';                          -- Ativa o reset
-        wait for period_time * 2;            -- Aguarda 2 ciclos de clock
-        rst <= '0';                          -- Desativa o reset
+        rst <= '1';
+        wait for period_time*2;
+        rst <= '0';
         wait;
     end process;
 
-    -- Tempo total de simulação
     sim_time_proc: process
     begin
-        wait for 10 us;                      -- Define o tempo total de simulação
-        finished <= '1';                     -- Marca o término da simulação
+        wait for 100 us;         -- <== TEMPO TOTAL DA SIMULACAO!!!
+        finished <= '1';
         wait;
-    end process;
+    end process sim_time_proc;
 
-    -- Geração do clock
     clk_proc: process
-    begin
+    begin                       -- gera clock até que sim_time_proc termine
         while finished /= '1' loop
             clk <= '0';
-            wait for period_time / 2;        -- Meio período do clock
+            wait for period_time/2;
             clk <= '1';
-            wait for period_time / 2;        -- Meio período do clock
+            wait for period_time/2;
         end loop;
         wait;
     end process clk_proc;
 
+    process
+    begin
+        wait for 200 ns;
+        
+        wait;
+    end process;
 end architecture;
